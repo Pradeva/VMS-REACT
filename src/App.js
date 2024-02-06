7df5c31e72baf8fe976b24b7470a5d283b7b204e
humbalang22 <humbalang22@gmail.com>
2024-02-06 15:35:47 +0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,22 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import io from 'socket.io-client';
 
 function App() {
+  let socket;
+  const handleButtonClick = (e) => {
+    const VE = document.getElementById("videoElement")
+        const mediaSource = new MediaSource();
+        const DataURL = URL.createObjectURL(mediaSource);
+        const Codec = "video/mp4; codecs=\"avc1.64001f\""
+        VE.src = DataURL;
+
+        mediaSource.addEventListener('sourceopen', function (e) {
+            const buffer = mediaSource.addSourceBuffer(Codec);
+            buffer.mode = 'sequence';
+            buffer.addEventListener('updateend', function (e) {
+                if (
+                    mediaSource.duration !== Number.POSITIVE_INFINITY &&
+                    VE.currentTime === 0 &&
+                    mediaSource.duration > 0
+                ) {
+                    VE.currentTime = mediaSource.duration - 1;
+                    mediaSource.duration = Number.POSITIVE_INFINITY;
+                }
+
+                VE.play();
+            });
+
+            socket = io('http://127.0.0.1:7878/', { path: '/streams/CAM-018'});
+            socket.on('segment', function (data) {
+                data = new Uint8Array(data);
+                buffer.appendBuffer(data);
+            });
+        });
+  }
+
+  // const handlestop = (e) => {
+  //   if (socket) {
+  //     const VE = document.getElementById("videoElement")
+  //     VE.pause();
+  //   }
+  // }
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <video className='Video-element' id="videoElement"></video>
+      <button className="Button-element" onClick={handleButtonClick}>Start</button>  
+      {/* <button className="Button-element" onClick={handlestop}>Stop</button>   */}
       </header>
     </div>
   );
